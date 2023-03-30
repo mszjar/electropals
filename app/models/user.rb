f369cfde3c1c3f46f133bfbbae1bc3693e98a2b0
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-03-30 15:12:16 +0200

    model start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :nickname, presence: true
+  validates :nickname, length: { maximum: 50 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
