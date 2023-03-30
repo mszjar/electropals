073e801078a2928b773ccb17ab0b7e37752968f6
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-03-30 16:05:32 +0200

    admin pundit

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,17 @@
+class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      user.admin? ? scope.all : scope.where(user: user)
+    end
+  end
+
+  def update?
+    record.user == user
+    # record: the restaurant passed to the `authorize` method in controller
+    # user: the `current_user` signed in with Devise
+  end
+
+  def destroy?
+    record.user == user || user.admin
+  end
+end
