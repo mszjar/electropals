3e27a2f4e251392a4a2f0f429ba2f70b24987aaf
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-03-30 15:52:06 +0200

    controller start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/show'
+  get 'comments/destroy'
+  get 'comments/update'
+  get 'comments/edit'
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/destroy'
+  get 'posts/update'
+  get 'posts/edit'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
