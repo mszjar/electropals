bf51cbeca710073a8b32740400baaac294587e9b
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-03-31 12:36:11 +0200

    nickname fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :nickname, presence: false
+  validates :nickname, presence: true
   validates :nickname, length: { maximum: 50 }
 
   # Include default devise modules. Others available are:
